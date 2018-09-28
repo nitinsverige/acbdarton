3a2742fa4c2afe7284127facc75b2c9a05bccf51
nitinsverige <datascience.nitinkumar@gmail.com>
2018-09-29 00:15:02 +0200

    Commit-01

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+
+
+#' abcdarton is just for test
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#'
+hello <- function() {
+  print("France won the 2018 Football World Cup")
+}
